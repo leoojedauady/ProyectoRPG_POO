ed50858a132d67f219fb695ccf4a1e78229a29ad
a23216396 <a23216396@FMAT-CC1-A6.inet.uady.mx>
2026-09-09 08:20:52 -0600

    Primera prueba de Commit y algunas clases

--- ProyectoRPG_POO/src/main/java/com/mycompany/proyectorpg_poo/ProyectoRPG_POO.java
+++ ProyectoRPG_POO/src/main/java/com/mycompany/proyectorpg_poo/ProyectoRPG_POO.java
@@ -12,6 +12,6 @@ package com.mycompany.proyectorpg_poo;
 public class ProyectoRPG_POO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo");
     }
 }
