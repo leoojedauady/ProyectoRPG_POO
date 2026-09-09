5d6ca3f60beee97466c2bba8ee1a08cf431bf5be
a23216396 <a23216396@FMAT-CC1-A6.inet.uady.mx>
2026-09-09 08:44:35 -0600

    Avances en clase Character y nuevas clases: Warrior y Mage

--- ProyectoRPG_POO/src/main/java/com/mycompany/proyectorpg_poo/Character.java
+++ ProyectoRPG_POO/src/main/java/com/mycompany/proyectorpg_poo/Character.java
@@ -5,11 +5,45 @@
 
 package com.mycompany.proyectorpg_poo;
 
-public class Character {
+public abstract class Character {
     private String name;
     private int level;
     private int health;
     private int maxHealth;
     private Inventory inventory;
+    private int experience;
+    private int strength;
+    private int defense;
+    private int status;
+
+    // constructor de character    
+    
+    
+    // métodos del character
+    abstract void attack(Character target);
+    // falta definir comportamiento
+
+    public abstract void defend(int damage);
+    // falta definir comportamiento
+
+    public void levelUp() {
+        System.out.println("Has subido de nivel");
+    }
+    
+    public boolean isAlive() {
+        // falta definir comportamiento
+        if (health>0) {
+            return true; // si su salud es mayor a 0 está vivo
+        } else
+        return false; // de lo contrario está muerto
+    }
+
+    // modificamos un toString() para que liste los stats
+    public String showStats() {
+        return "Character{" + "name=" + name + ", level=" + level + ", health=" + health + ", maxHealth=" + maxHealth + ", inventory=" + inventory + ", experience=" + experience + ", strength=" + strength + ", defense=" + defense + ", status=" + status + '}';
+    }
+    
+    
+    
     
 }
---
+++ ProyectoRPG_POO/src/main/java/com/mycompany/proyectorpg_poo/Mage.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectorpg_poo;
+
+/**
+ *
+ * @author LeoMija
+ */
+public class Mage extends Character {
+
+    @Override
+    void attack(Character target) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void defend(int damage) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
